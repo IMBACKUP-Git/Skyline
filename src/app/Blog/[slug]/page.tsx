4dbf00db011d98f2454e3729bf564a4aa3a1dcd6
IMBACKUP-Git <veer@integramagna.com>
2026-09-21 17:27:31 +0530

    Fix dynamic blog build

--- src/app/Blog/[slug]/page.tsx
+++ src/app/Blog/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { notFound } from "next/navigation";
 import { blogs } from "@/data/blog";
-import BlogInternal from "@/app/BlogInternal/page";
+import BlogInternal from "@/app/BlogInternal/BlogInternal";
 
 export default async function BlogPage({
   params,
